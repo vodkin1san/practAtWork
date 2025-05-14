01fd395980b5a87b7c43a4596666acdda00f1608
Вячеслав Ермолович <ms.focs@gmail.com>
2025-05-14 12:51:48 +0300

    Очистка ветки develop

--- js/utils.js
+++
@@ -1,8 +0,0 @@
-export function updateKeyboardLabels(currentLang) {
-  const keys = document.querySelectorAll(".key");
-  keys.forEach((key) => {
-    if (key.hasAttribute("data-ru") && key.hasAttribute("data-en")) {
-      key.textContent = key.getAttribute("data-" + currentLang);
-    }
-  });
-}
